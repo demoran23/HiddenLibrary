8531a570ce92860159a932d8194e4673d35dced7
Adam Skinner <adam.skinner@gmail.com>
2022-02-20 10:26:26 -0500

    Framework 7 > Appbar / icons (vanilla)

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Hidden Library
+
+An application to manage / view manga.
+
+---
+
 # Svelte + TS + Vite
 
 This template should help get you started developing with Svelte and TypeScript in Vite.
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import App from './App.svelte'
+import Framework7 from 'framework7/lite-bundle';
+import Framework7Svelte from 'framework7-svelte';
+
+Framework7.use(Framework7Svelte);
 
 const app = new App({
   target: document.getElementById('app')
